75a2c28f7616489b21377ccc56b334b854c9d591
Martí Paredes <martiparedes19@gmail.com>
2022-11-05 12:27:29 +0100

    PRE
    Enunciat de la práctica + codi inicial propocionat per el  professor.

---
+++ agent.py
@@ -0,0 +1,22 @@
+"""
+
+ClauPercepcio:
+    POSICIO = 0
+    OLOR = 1
+    PARETS = 2
+"""
+from ia_2022 import entorn
+from practica1 import joc
+
+
+class Rana(joc.Rana):
+    def __init__(self, *args, **kwargs):
+        super(Rana, self).__init__(*args, **kwargs)
+
+    def pinta(self, display):
+        pass
+
+    def actua(
+            self, percep: entorn.Percepcio
+    ) -> entorn.Accio | tuple[entorn.Accio, object]:
+        pass
